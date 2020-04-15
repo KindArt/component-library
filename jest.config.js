0b90d2f380131e4aa5f971b84362bafc3783316a
Tristan Cobourne <tristan@kindart.co.uk>
2020-04-15 19:22:28 +0100

    removed stories

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   setupFiles: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
-  modulePaths: ['<rootDir>'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
 };
--- src/components/Toggle/toggle.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Toggle from '.';
-
-export default {
-  component: Toggle,
-  title: 'Toggle'
-};
-
-export const normal = () => <Toggle name="normal" />;
